76380143bdc2325ff078da64e3e1b96aab9a54fb
Luis Pedro Coelho <luis@luispedro.org>
2014-02-06 22:10:26 +0100

    RFCT Break up long code line

--- annex-hashes.py
+++ annex-hashes.py
@@ -3,6 +3,7 @@ import re
 from os import path
 for line in os.popen("git ls-tree git-annex --full-tree -r"):
     tokens = line.split()
-    m = re.search('^SHA256E-[^-]+--([0-9a-f]+)\..*\.log$', path.basename(tokens[-1]))
+    name = path.basename(tokens[-1])
+    m = re.search('^SHA256E-[^-]+--([0-9a-f]+)\..*log$', name)
     if m:
         print m.group(1)
