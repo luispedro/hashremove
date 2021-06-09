ba32ad070a357c4930f6a1c582f43664aa3230e8
Luis Pedro Coelho <luis@luispedro.org>
2021-06-09 12:19:04 +0200

    BUG Fix regex encoding

--- annex-hashes.py
+++ annex-hashes.py
@@ -4,6 +4,6 @@ from os import path
 for line in os.popen("git ls-tree git-annex --full-tree -r"):
     tokens = line.split()
     name = path.basename(tokens[-1])
-    m = re.search('^SHA256E-[^-]+--([0-9a-f]+)\..*log$', name)
+    m = re.search(r'^SHA256E-[^-]+--([0-9a-f]+)\..*log$', name)
     if m:
         print(m.group(1))
